ffb33324d0e5a46294ee4df40e1c8f27cfab1703
Sravankumarviswanadhuni <143478144+Sravankumarviswanadhuni@users.noreply.github.com>
2023-08-31 15:19:55 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "sravan kumar viswanadhuni!" );
+        System.out.println( "sravanI!" );
     }
 }
